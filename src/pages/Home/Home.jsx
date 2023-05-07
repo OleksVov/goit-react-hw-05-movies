480dcfce89d091c2661b7058c767bddcad890e58
Oleksandr <alex.vovchok@gmail.com>
2023-05-07 11:41:32 +0300

    Home start

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,17 @@
-
+import { useEffect, useState } from "react";
+import { fetchTrending } from "service/api";
 export const Home = () => {
+const [movies, setMovies] = useState([]);
+
+useEffect(() => {
+    async function getMovies() {
+        const result = await fetchTrending();
+        setMovies(result);
+        console.log(result);
+    }
+    getMovies();
+},[]);
+
     return (
         <h1>Home</h1>
     )
--- src/service/api.jsx
+++ src/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-API_KEY = '8d83f7efdfaf711afea4c7df39bd5119';
+const API_KEY = '8d83f7efdfaf711afea4c7df39bd5119';
 
 // https://api.themoviedb.org/3/movie/550?api_key=8d83f7efdfaf711afea4c7df39bd5119
 // /trending/get-trending 
@@ -11,6 +11,6 @@ API_KEY = '8d83f7efdfaf711afea4c7df39bd5119';
 // /movies/get-movie-reviews запрос обзоров для страницы кинофильма
 
 export const fetchTrending = async() => {
-const response = await axios.get(`/trending/get-trending?api_key=${API_KEY}`);
+const response = await axios.get(`/trending/movie/week?api_key=${API_KEY}`);
 return response;
 }
